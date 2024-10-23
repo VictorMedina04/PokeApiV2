cb250f11e3445d962c210b7ec3ea0453cd7e615c
Manuel Gomez <gomez.maman24@triana.salesianos.edu>
2024-10-23 12:25:47 +0200

    Lista casi hecha

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon, PokemonListResponse } from '../../interfaces/pokemon.interfaces';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.css'
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit {
+
+  listadoPokemon: Pokemon[] = [];
+
+  constructor(private pokemonService: PokemonService) { }
+
+
+
+  ngOnInit(): void {
+    this.pokemonService.getPokemonlist()
+      .subscribe(pokemonListResult => {
+        this.listadoPokemon = pokemonListResult.results;
+      });
+  }
 
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PokemonListResponse } from '../interfaces/pokemon.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  getPokemonlist(): Observable<PokemonListResponse> {
+    return this.http.get<PokemonListResponse>('https://pokeapi.co/api/v2/pokemon');
+  }
 }
