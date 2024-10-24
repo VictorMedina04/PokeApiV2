bd13fa39854adee97bf893b3598833c223351500
Manuel Gomez <gomez.maman24@triana.salesianos.edu>
2024-10-24 12:30:32 +0200

    acabando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
-import { not } from 'rxjs/internal/util/not';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { ItemComponent } from './components/item/item.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 
 const routes: Routes = [
   { path: 'pokemon-list', component: PokemonListComponent },
   { path: 'items', component: ItemComponent },
+  {path: 'pokemon-details/:id', component: PokemonDetailsComponent },
   { path: '', redirectTo: '/pokemon-list', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
 import { PokemonDetailsResponse } from '../../interfaces/pokemon-detail.interfaces';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -12,13 +13,20 @@ export class PokemonDetailsComponent implements OnInit {
   @Input() pokemonId: number | undefined;
   pokemon: PokemonDetailsResponse | undefined;
 
-  constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+
+    this.pokemonId = Number(this.route.snapshot.paramMap.get('id'));
+
 
     this.pokemonService.getOnePokemon(this.pokemonId!).subscribe(pokemonResults => {
       this.pokemon = pokemonResults;
     });
 
   }
+
+  
 }
