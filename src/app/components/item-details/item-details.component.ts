113ef9c8e0ae1bcc5b0863cd8408a136fc153799
Victor <medina.levic24@triana.salesianos.edu>
2024-10-25 10:43:53 +0200

    detalles de items finiquitaos

--- src/app/components/item-details/item-details.component.ts
+++ src/app/components/item-details/item-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ItemDetailsResponse } from '../../interfaces/item-details.interface';
 import { ItemService } from '../../services/item.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-details',
@@ -12,11 +13,17 @@ export class ItemDetailsComponent implements OnInit {
   @Input() itemId: number | undefined;
   item: ItemDetailsResponse | undefined;
 
-  constructor(private itemService: ItemService) { }
+  constructor(private itemService: ItemService,private route: ActivatedRoute,
+    private router: Router) { 
+    
+  }
 
 
 
   ngOnInit(): void {
+    
+    this.itemId= Number(this.route.snapshot.paramMap.get('id'));
+
     this.itemService.getOneItem(this.itemId!).subscribe((response) => {
       this.item = response;
     });
@@ -31,5 +38,25 @@ export class ItemDetailsComponent implements OnInit {
 
   formatOrder(order: number): string {
     return `#${order.toString().padStart(4, '0')}`;
+  }
+
+  goToNextItem(): void {
+    const nextItemId = (this.item?.id || 0) + 1;
+    this.router.navigateByUrl(`/item-details/${nextItemId}`).then(() => {
+      window.location.reload();
+    });
+  }
+
+  goToPreviousItem(): void {
+    const nextItemId = (this.item?.id || 0) - 1;
+
+    if (nextItemId < 1) {
+      this.router.navigateByUrl(`/item-list`).then(() => { window.location.reload(); });
+    } else {
+      this.router.navigateByUrl(`/item-details/${nextItemId}`).then(() => {
+        window.location.reload();
+
+      });
+    }
   }
 }
